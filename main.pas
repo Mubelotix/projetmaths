320229de802b4bb7af21a38cdf71af611d16d3eb
Simon Girard <simon.girard@insa-rouen.fr>
2022-11-22 17:51:34 +0100

    Read b

--- main.pas
+++ main.pas
@@ -79,6 +79,14 @@ begin
     end;
 
     read_input_data.a.display();
+
+    for y := 0 to size - 1 do begin
+        readln(line);
+        cursor := 1;
+        read_input_data.b.set_fraction(y, read_number(line, cursor));
+    end;
+
+    read_input_data.b.display();
 end;
 
 begin
