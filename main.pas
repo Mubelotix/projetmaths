9a81b460de8a0cbeb0b19fcce8e5d32e60624bbd
Mubelotix <mubelotix@gmail.com>
2022-11-27 19:54:48 +0100

    Rename variable

--- main.pas
+++ main.pas
@@ -2,18 +2,18 @@ program linear_equation_solver;
 
 uses matrix, fraction, sysutils, crt, termio, io;
 
-var input_data: InputData;
+var data: InputData;
 
 begin
-    input_data := read_input_data();
+    data := read_input_data();
     ClrScr();
-    apply_gauss(input_data.a, input_data.b);
+    apply_gauss(data.a, data.b);
 
     // Affiche les résultats ou les retourne dans le fichier
     if isatty(output)=1 then begin
         writeln('Terminé!');
-        input_data.a.display();
-        input_data.b.display();
+        data.a.display();
+        data.b.display();
     end else
-        output_data(input_data);
+        output_data(data);
 end.
