b9d7ee249547c70c541ef9ddcaebc83b8f74a7db
Mubelotix <mubelotix@gmail.com>
2022-11-27 18:19:22 +0100

    Move io to file

---
+++ io.pas
@@ -0,0 +1,97 @@
+unit io;
+
+interface
+
+uses fraction, matrix, sysutils;
+
+// Ce qui est lu en entrée du programme
+type InputData = record
+    a: SquareMatrix;
+    b: ColumnMatrix;
+end;
+
+// Fonction qui lit les données d'entrée
+function read_input_data(): InputData;
+
+implementation
+
+function read_integer(line: String; var cursor: Integer): Integer;
+var start, len: Integer;
+    str: String;
+begin
+    // Ignore tous les espaces
+    while (cursor <= length(line)) and (line[cursor] = ' ') do
+        cursor := cursor + 1;
+    // Vérifie qu'on ne manque pas de caractères
+    if cursor > length(line) then begin
+        writeln('Nombre manquant');
+        exit;
+    end;
+    // On lit désormais le numérateur, collectons tous les caractères qui composent le numérateur (jusqu'au prochain espace ou barre de division)
+    start := cursor;
+    while (cursor <= length(line)) and (line[cursor] <> ' ') and (line[cursor] <> '/') do
+        cursor := cursor + 1;
+    len := cursor - start;
+    // On vérifie que le numérateur contient quelquechose
+    if len = 0 then begin
+        writeln('Nombre vide');
+        exit;
+    end;
+    // On parse le numérateur en nombre
+    str := copy(line, start, len);
+    read_integer := StrToInt(str);
+    // Ignore tous les espaces
+    while (cursor <= length(line)) and (line[cursor] = ' ') do
+        cursor := cursor + 1;
+end;
+
+function read_number(line: String; var cursor: Integer): Number;
+var num, den: Integer;
+begin
+    // Lit le numérateur
+    num := read_integer(line, cursor);
+
+    // Termine si le dénominateur n'est pas présent ou sinon passe le /
+    if (cursor > length(line)) or (line[cursor] <> '/') then begin
+        read_number.init_int(num);
+        exit;
+    end else
+        cursor := cursor + 1;
+
+    // Lit le dénominateur
+    den := read_integer(line, cursor);
+    read_number.init(num, den);
+end;
+
+function read_input_data(): InputData;
+var line: String;
+    size: Integer;
+    cursor, start, len: Integer;
+    x, y: Integer;
+    str: String;
+    num, den: Integer;
+    fraction: Number;
+begin
+    readln(size);
+    read_input_data.a.init(size);
+    read_input_data.b.init(size);
+
+    for y := 0 to size - 1 do begin
+        readln(line);
+        cursor := 1;
+        for x := 0 to size - 1 do
+            read_input_data.a.set_fraction(x, y, read_number(line, cursor));
+    end;
+
+    read_input_data.a.display();
+
+    for y := 0 to size - 1 do begin
+        readln(line);
+        cursor := 1;
+        read_input_data.b.set_fraction(y, read_number(line, cursor));
+    end;
+
+    read_input_data.b.display();
+end;
+
+end.
